2856bf23b67f414ed6622325bd860c7e2de10509
miche kazadi <michemubikay@gmail.com>
2026-03-12 02:10:55 +0100

    modif websoket

--- backEnd/backEnd/settings.py
+++ backEnd/backEnd/settings.py
@@ -26,16 +26,22 @@ SECRET_KEY = 'django-insecure-abehpu(5_jgyu-gpvg6@e2lc1loubr*b@xxzdxg%=zxc@nu5^u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-# Dans settings.py
-ALLOWED_HOSTS = [
-    'miche-kazadi-djangowhat-sppclone-1.onrender.com',
-    '127.0.0.1',
-    'localhost',
-    '0.0.0.0',
-    '192.168.0.100',
-    'miche-kazadi-djangowhat-sppclone.onrender.com', # Ajoute aussi ton adresse IP locale ici !
-]
+
+# --- Sécurité ---
+SECRET_KEY = os.environ.get('SECRET_KEY', 'une-cle-par-defaut-pour-le-test')
+
+# --- Redis ---
+# On ne met pas l'URL en dur, on lit la variable d'environnement
+REDIS_URL = os.environ.get('REDIS_URL')
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [REDIS_URL],
+        },
+    },
+}
 # Application definition
 
 INSTALLED_APPS = [
